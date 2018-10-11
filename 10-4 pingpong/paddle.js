eb9a96c776331b9e7298bd2221f8f70fbf72583d
GABE795342 <795342@seq.org>
2018-10-11 08:45:12 -0700

    10-11 pong program commit
    10-11

---
+++ 10-4 pingpong/paddle.js
@@ -0,0 +1,32 @@
+var x = 0;
+var y = 500
+
+
+//PADDLE FUNCTION
+function Paddle(location, velocity, col){
+	this.loc = location;
+	this.vel = velocity;
+	// from ball func
+	//this.rad = radius;
+	this.col = col;
+	this.acc = createVector(0,.1);
+}
+
+this.run = function(){
+	this.checkEdges();
+	this.update();
+	this.render();
+}
+	
+	
+this.vel.limit(3)
+	this.loc.add(this.vel);
+
+}
+this.checkEdges = function(){
+	if(this.loc.x < 0) this.vel.x = -this.vel.x;
+	if(this.loc.x > width) this.vel.x = -this.vel.x;
+	if(this.loc.y < 0) this.vel.y = -this.vel.y;
+	if(this.loc.y > height) this.vel.y = -this.vel.y;
+}
+	
--- 10-4 pingpong/sketch.js
+++ 10-4 pingpong/sketch.js
@@ -6,14 +6,17 @@
 **
 */
  
-
+var Balls = [];
+var paddle;
 
 //  The setup function function is called once when your program begins
 function setup() {
   var cnv = createCanvas(800, 800);
   cnv.position((windowWidth-width)/2, 30);
-  background(255, 255, 0);
+  background(255, 100, 0);
   fill(200, 30, 150);
+  loadBalls(10)
+  paddle= new paddle(createVector(width/2, height/2, createVector(1,2), 25, color(255,0,0)))
 
 
 }
