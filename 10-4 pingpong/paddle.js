0576d03eeff2ecc55ef9cd8e2babdf6ca7e509b4
GABE795342 <795342@seq.org>
2018-10-16 08:49:40 -0700

    10-16
    10-16 1

--- 10-4 pingpong/paddle.js
+++ 10-4 pingpong/paddle.js
@@ -1,16 +1,25 @@
 var x = 0;
 var y = 500
-
+var wid = 50;
+var ht = 20;
 
 //PADDLE FUNCTION
-function Paddle(location, velocity, col){
+function Paddle(location, col){
 	this.loc = location;
-	this.vel = velocity;
+	//this.vel = velocity;
 	// from ball func
 	//this.rad = radius;
 	this.col = col;
-	this.acc = createVector(0,.1);
+	//this.acc = createVector(0,.1);
+	
+	
 }
+
+function draw() {
+	rect(
+
+	
+	}
 
 this.run = function(){
 	this.checkEdges();
