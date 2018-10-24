8d0d39cca5460ae2c91a189d228cc3e58799e442
GABE795342 <795342@seq.org>
2018-10-23 20:43:33 -0700

    10-23 bubble sort
    bubble sort

---
+++ 10-22 wordsort/sketch.js
@@ -0,0 +1,36 @@
+/* Bubble sorting scheme
+**
+** 10-23-18
+**
+*/
+
+
+var randonumbs; //random number generator scheme
+var numarray = []; //number array
+var right; // right value for bubble comparitor
+var left; //left value for bubble for comparitor
+var medium; //medium for moving the two values
+var sorted; //flag var indicating if numbers have been sorted, 1=true 0=false
+
+ for(var i = 0; i < 50; i++){ //for loop with 50 being the number of random numbers
+	 
+	 var randonumbs = int(random(0, 1000)); // generates random number between 0 and 1000
+	 numarray.push(randonumbs);
+	 
+	 
+	 
+	
+	 
+	 
+ }
+
+ 
+ //sorting least to greatest
+ 
+if(sorted = 0){
+	if(left > right){ //if left greater than right
+		var medium = var left;
+		var left = var right;
+		var right = var medium;
+	}
+ }
