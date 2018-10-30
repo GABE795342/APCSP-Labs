5a391f5f51b422c32ca4c2a319b68c63ff712113
GABE795342 <795342@seq.org>
2018-10-30 08:48:50 -0700

    10-30 initial
    10-30

--- 10-26 ColorBars/sketch.js
+++ 10-26 ColorBars/sketch.js
@@ -6,11 +6,12 @@ var Bars = [];
 //  This is a comment
 //  The setup function function is called once when your program begins
 function setup() {
-  //var cnv = createCanvas(800, 800);
- // cnv.position((windowWidth-width)/2, 30);
-  //background(5, 5, 5);
- // fill(200, 30, 150);
+  var cnv = createCanvas(800, 800);
+  cnv.position((windowWidth-width)/2, 30);
+  background(5, 5, 5);
+  fill(200, 30, 150);
   numBars = 100;
+  
   loadBars(numBars);
 
 
@@ -51,7 +52,7 @@ function loadBars(numBars){
 
 
 
-myBubbleSort(){
+function myBubbleSort(){
 	
 	
 	
