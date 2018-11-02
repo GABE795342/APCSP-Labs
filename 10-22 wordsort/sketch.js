852ee69a968a4d59c6485cd913c0bea6a0cba067
GABE795342 <795342@seq.org>
2018-11-01 21:07:41 -0700

    11-1 late commit
    11-1

--- 10-22 wordsort/sketch.js
+++ 10-22 wordsort/sketch.js
@@ -10,27 +10,53 @@ var numarray = []; //number array
 var right; // right value for bubble comparitor
 var left; //left value for bubble for comparitor
 var medium; //medium for moving the two values
-var sorted; //flag var indicating if numbers have been sorted, 1=true 0=false
+var sorted; //flag var indicating if numbers have been sorted, 1=true 0=false //unused
+function setup(){
+   
+    bubbleSort();
+}
 
- for(var i = 0; i < 50; i++){ //for loop with 50 being the number of random numbers
-	 
-	 var randonumbs = int(random(0, 1000)); // generates random number between 0 and 1000
-	 numarray.push(randonumbs);
-	 
-	 
-	 
-	
-	 
-	 
- }
+//old code for number comparison
+ //for(var i = 0; i < 50; i++){ //for loop with 50 being the number of random numbers
+	 //var randonumbs = int(random(0, 1000)); // generates random number between 0 and 1000
+	// numarray.push(randonumbs);
+ //}
 
  
  //sorting least to greatest
  
-if(sorted = 0){
-	if(left > right){ //if left greater than right
-		var medium = var left;
-		var left = var right;
-		var right = var medium;
+ 
+ 
+ 
+//if(sorted = 0){
+function mybubblesort(txt){
+	var wordlength = txt.length;
+	for (var a = 0; a < length; a++){
+		for (var b = 0; b<(length - a - 1); b++){
+			if(txt[b] > txt[b+1]){
+				var medium = txt[b];
+				txt[b] = txt[b+1];
+				txt[b+1] = medium;
+				
+				
+				
+			}
+			
+			
+			
+		
+		
+		
+		}
 	}
- }
+	
+	
+	
+	
+}
+//	if(left > right){ //if left greater than right
+	//	var medium = var left;
+		//var left = var right;
+		//var right = var medium;
+	//}
+ //}
