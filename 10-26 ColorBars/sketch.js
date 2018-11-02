0101710e15b527f1a77433fda559728904827f6b
GABE795342 <795342@seq.org>
2018-11-01 21:20:20 -0700

    11-1 final
    final

--- 10-26 ColorBars/sketch.js
+++ 10-26 ColorBars/sketch.js
@@ -7,19 +7,34 @@ var left; //left value for bubble for comparitor
 var medium; //medium for moving the two values
 var sorted; //flag var indicating if numbers have been sorted, 1=true 0=false
 
+var RR;
+var GG;
+var BB;
+
 
 
 //  This is a comment
 //  The setup function function is called once when your program begins
 function setup() {
-  var cnv = createCanvas(800, 800);
+  var cnv = createCanvas(1000, 1000);
   cnv.position((windowWidth-width)/2, 30);
   background(5, 5, 5);
   fill(200, 30, 150);
   numBars = 100;
   
   loadBars(numBars);
-
+  //loadBars();
+  for(var j = 0; j<2; j++){
+    Xloc = 0;
+    for(var i = 0; i<colorBars.length; i++){
+      colorBars[i].render(Xloc, Yloc);
+      Xloc = Xloc +20;
+    }
+    sortBar(colorBars);
+    Yloc = Yloc+50;
+  }
+  console.log(colorBars);
+}
 
 }
 
